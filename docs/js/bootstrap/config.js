7f68e4a20f64073ef65dc4d6e00dcd24b3be9bdc
Des1red <justbeasteasy@gmail.com>
2026-03-07 09:45:33 -0500

    fixed url

--- docs/js/bootstrap/config.js
+++ docs/js/bootstrap/config.js
@@ -2,9 +2,8 @@ let CONFIG = null;
 
 export async function loadConfig() {
 
-//   const resp = await fetch("https://seeall.onrender.com/config");
-   const resp = await fetch("http://192.168.1.110:8080/config");
-CONFIG = await resp.json();
+   const resp = await fetch("https://seeall.onrender.com/config");
+   CONFIG = await resp.json();
 }
 
 export function getAPI() {
